c142fe30e2423e48cd0c2b77b95be0fd4e8ddcde
Alexander Novikov <alexnovikov02@gmail.com>
2019-02-01 11:32:19 +0200

    remove redundant generateId method

--- server/services/authSrv.js
+++ server/services/authSrv.js
@@ -1,9 +1,5 @@
 const authSrv = {
 
-  generateId() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-  },
-
   isEmailValid(email) {
     const regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexp.test(email);
