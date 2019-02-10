636cb8edd6dc8c9fca6f7653ba4f5b535d1212c2
Alexander Novikov <alexnovikov02@gmail.com>
2019-02-10 21:06:31 +0200

    clean up router and redundant components

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
-import About from '@/components/About';
-import AuthorizePage from '@/components/AuthorizePage';
-import Profile from '@/components/Profile';
-import Users from '@/components/Users';
-import UserDetail from '@/components/UserDetail';
 
 Vue.use(Router);
 
@@ -16,32 +11,6 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About,
-    },
-    {
-      path: '/authorize',
-      name: 'AuthorizePage',
-      component: AuthorizePage,
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: Profile,
-    },
-    {
-      path: '/users',
-      name: 'Users',
-      component: Users,
-    },
-    {
-      path: '/user-detail/:id',
-      name: 'UserDetail',
-      component: UserDetail,
-      props: true,
     },
   ],
 });
